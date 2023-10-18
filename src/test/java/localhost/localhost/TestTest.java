baeb85133623c89c25fcfdfe4bb4aecdacd58419
Jim Bennett <jimbobbennett@liblab.com>
2023-10-18 12:24:12 -0700

    SDK update generated by liblab

---
+++ src/test/java/localhost/localhost/TestTest.java
@@ -0,0 +1,14 @@
+package localhost.localhost;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.*;
+
+public class TestTest {
+
+  @Test
+  @DisplayName("Tests that JUnit is working")
+  public void test() {
+    assertTrue(true);
+  }
+}
